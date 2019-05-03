311cfc0895e4b14486108d00977a3b63286812c7
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-02 22:38:40 -0300

    Projeto Inicializado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
