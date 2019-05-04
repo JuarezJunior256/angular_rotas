d97d309e0ac18d80c2711a4cccb3f6ecc46db42a
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-04 19:09:07 -0300

    Refatoração do código pra criação do modulo de rotas

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { RouterModule } from '@angular/router';
+
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { CursosRoutingModule } from './cursos.routing.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    CursosRoutingModule
+    // RouterModule
   ],
   exports: [],
   declarations: [
