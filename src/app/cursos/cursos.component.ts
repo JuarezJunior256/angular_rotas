ca50006a4894aa1cecd90ad6d650351ea133d42a
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-03 23:47:02 -0300

    Extraindo parametros de url

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CursosService } from './cursos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
-
+export class CursosComponent implements OnInit, OnDestroy {
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(private cursosService: CursosService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+      // tslint:disable-next-line:no-string-literal
+      this.pagina = queryParams['pagina'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    this.pagina++;
+    // tslint:disable-next-line:object-literal-key-quotes
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
