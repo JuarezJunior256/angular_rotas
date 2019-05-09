c60ecfb450fa95a08bbfc0bbb006f226cdcab249
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-05-09 00:38:10 -0300

    Criando guards rotas com CanLoad

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../login/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | boolean {
-      if (this.auth.usuarioEstaAutenticado()) {
-        return true;
-      } else {
-        this.router.navigate(['/login']);
-        return false;
-      }
+     return this.verificarAcesso();
+  }
+
+  private verificarAcesso() {
+    if (this.auth.usuarioEstaAutenticado()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verificarAcesso();
   }
 }
